4bd28a4bc59f0567141c4fa73373ab2745699699
GemsZen <GemsZen@users.noreply.github.com>
2023-04-25 18:21:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gerador-senha
+ Um gerador de senha de 16 caracteres
---
+++ gerador-senha.py
@@ -0,0 +1,12 @@
+import random
+
+baixo = "abcdefghijklmnopqrstuvwxyz"
+alto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numeros = "0123456789"
+simbolos = "[]{}()*;/,._-"
+
+all = baixo + alto + numeros + simbolos
+length = 16
+senha = "".join(random.sample(all,length))
+
+print(senha)
